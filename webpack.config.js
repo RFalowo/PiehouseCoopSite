b775689bc7a618b3435abb3dfb404b0d960bd373
Oluremi Falowo <45524749+RFalowo@users.noreply.github.com>
2024-11-12 16:37:06 +0000

    using webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.fbx$/,
+        loader: 'three-fbx-loader',
+      },
+    ],
+  },
+};
