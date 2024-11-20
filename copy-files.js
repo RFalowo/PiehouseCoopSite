c28648053282cd821b38b8d8b6d01a5d04a729c9
Oluremi Falowo <45524749+RFalowo@users.noreply.github.com>
2024-11-20 11:42:23 +0000

    Trigger redeployment

--- copy-files.js
+++ copy-files.js
@@ -4,7 +4,6 @@ const path = require('path');
 const files = [
     'texture-pie-18701720.webp',
     'Pie_Goblin_1110143250.fbx',
-    'index.html',
 ];
 for (const file of files) {
     const srcFile = path.join(__dirname, file);
